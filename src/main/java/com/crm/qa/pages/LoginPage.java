a9829e175f1cf9178cd6c32e886090e72ea428d7
Srinu Kusumanchi <SK00493688@techmahindra.com>
2020-06-14 22:37:41 +0530

    Intial commit for selenium project

---
+++ src/main/java/com/crm/qa/pages/LoginPage.java
@@ -0,0 +1,55 @@
+package com.crm.qa.pages;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.crm.qa.base.TestBase;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginPage extends TestBase{
+	
+	//Page Factory - OR:
+	@FindBy(name="username")
+	WebElement username;
+	
+	@FindBy(name="password")
+	WebElement password;
+	
+	@FindBy(xpath="//input[@type='submit']")
+	WebElement loginBtn;
+	
+	@FindBy(xpath="//button[contains(text(),'Sign Up')]")
+	WebElement signUpBtn;
+	
+	@FindBy(xpath="//font[text()='crm']")
+	WebElement crmLogo;
+	
+	//Initializing the Page Objects:
+	public LoginPage(){
+		PageFactory.initElements(driver, this);
+	}
+	
+	//Actions:
+	public String validateLoginPageTitle(){
+		return driver.getTitle();
+	}
+	
+	public boolean validateCRMImage() throws InterruptedException {
+		Thread.sleep(7000);
+		return crmLogo.isDisplayed();
+	}
+	
+	public HomePage login(String un, String pwd){
+		username.sendKeys(un);
+		password.sendKeys(pwd);
+		//loginBtn.click();
+		    	JavascriptExecutor js = (JavascriptExecutor)driver;
+		    	js.executeScript("arguments[0].click();", loginBtn);
+		    	
+		return new HomePage();
+	}
+	
+}
